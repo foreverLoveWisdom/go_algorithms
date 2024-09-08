81498aef650db99979e20313a37d41824db51da9
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-08 18:01:48 +0700

    chore: remove unnecessary comments and functions in pairsUnsortedArr function

--- pairs_unsorted_arr.go
+++ pairs_unsorted_arr.go
@@ -1,33 +1,16 @@
 package main
 
-func flatten(arr [][]int) []int {
-	var flat []int
-	for _, subArr := range arr {
-		flat = append(flat, subArr...)
-	}
-	return flat
-}
-
-func contains(arr []int, target int) bool {
-	for _, elem := range arr {
-		if elem == target {
-			return true
-		}
-	}
-	return false
-}
-
 func pairsUnsortedArr(nums []int, target int) [][]int {
 	result := [][]int{}
 	used := make(map[int]bool) // Map to keep track of used indices
 
 	for i, num := range nums {
 		if used[i] {
-			continue // Skip if the index is already used
+			continue
 		}
 		for j := i + 1; j < len(nums); j++ {
 			if used[j] {
-				continue // Skip if the index is already used
+				continue
 			}
 			if num+nums[j] == target {
 				result = append(result, []int{i, j})
