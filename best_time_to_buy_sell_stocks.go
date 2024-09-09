c4962cd5719f6d5871ca156a2ef55279a652ff1e
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-09-09 21:12:37 +0700

    refactor: improve variable naming and remove unnecessary imports and comments

--- best_time_to_buy_sell_stocks.go
+++ best_time_to_buy_sell_stocks.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"log"
-)
-
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
 // You want to maximize your profit by choosing a single day to buy one stock and
@@ -34,14 +30,14 @@ func maxProfit(prices []int) int {
 	maxProfit := 0        // Keep track of the maximum profit
 
 	for i := 1; i < len(prices); i++ {
-		// Calculate profit if we sell on day i
-		profit := prices[i] - minPrice
+		currentPrice := prices[i]
+		profit := currentPrice - minPrice
 		if profit > maxProfit {
 			maxProfit = profit // Update max profit if current profit is greater
 		}
 		// Update minPrice if we find a lower price (better buy day)
-		if prices[i] < minPrice {
-			minPrice = prices[i]
+		if currentPrice < minPrice {
+			minPrice = currentPrice
 		}
 	}
 	return maxProfit
